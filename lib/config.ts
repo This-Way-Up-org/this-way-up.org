0e89da2a42e18177ed516cefde065195639de8c2
Offren <OffrenLLC@outlook.com>
2024-10-30 13:15:52 -0500

    adding initial setup instruction comments

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,11 @@
 export const GITHUB_REPO = {
-  owner: 'Offren',
-  name: 'gitWiki',
-  branch: 'main',
-  contentPath: 'content'
-}
-
-export const getGithubEditUrl = (path: string): string => {
-  return `https://github.com/${GITHUB_REPO.owner}/${GITHUB_REPO.name}/tree/${GITHUB_REPO.branch}/${GITHUB_REPO.contentPath}/${path}`
-}
+    owner: 'Offren',  // replace this with your Github Username
+    name: 'gitWiki', // replace this with your repo's name
+    branch: 'main',
+    contentPath: 'content'
+  }
+  
+  export const getGithubEditUrl = (path: string): string => {
+    return `https://github.com/${GITHUB_REPO.owner}/${GITHUB_REPO.name}/tree/${GITHUB_REPO.branch}/${GITHUB_REPO.contentPath}/${path}`
+  }
+  
