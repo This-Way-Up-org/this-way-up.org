1195e69644615966fd78e25ca4cd8c144cc510e4
Offren <OffrenLLC@outlook.com>
2024-10-30 13:01:38 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import { GetStaticProps } from 'next'
 import Layout from '../components/Layout'
 import { getAllPages } from '../lib/wiki'
+import { getGithubEditUrl } from '../lib/config'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import { marked } from 'marked'
 
-export default function Home({ content, pages }) {
+interface HomeProps {
+  content: string
+  pages: any[]
+  githubUrl: string
+}
+
+export default function Home({ content, pages, githubUrl }: HomeProps) {
   return (
-    <Layout pages={pages}>
-      <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: content }} />
+    <Layout pages={pages} githubUrl={githubUrl}>
+      <article className="wiki-article">
+        <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: content }} />
+      </article>
     </Layout>
   )
 }
@@ -20,11 +29,13 @@ export const getStaticProps: GetStaticProps = async () => {
   const fileContents = fs.readFileSync(readmePath, 'utf8')
   const { content } = matter(fileContents)
   const htmlContent = marked(content)
+  const githubUrl = getGithubEditUrl('README.md')
 
   return {
     props: {
       content: htmlContent,
-      pages
+      pages,
+      githubUrl
     }
   }
 }
