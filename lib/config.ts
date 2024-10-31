874599a591a29ec977c5d3854d78848c9f20ed42
Offren <OffrenLLC@outlook.com>
2024-10-30 21:42:10 -0500

    made the Talk button URL configurable.

--- lib/config.ts
+++ lib/config.ts
@@ -5,6 +5,9 @@ export const GITHUB_REPO = {
   contentPath: 'content'
 }
 
+// Configure your discussion/talk URL here
+export const TALK_URL = 'https://github.com/Offren/gitWiki/discussions' // replace this with whatever site you want
+
 export const getGithubEditUrl = (path: string): string => {
   return `https://github.com/${GITHUB_REPO.owner}/${GITHUB_REPO.name}/edit/${GITHUB_REPO.branch}/${GITHUB_REPO.contentPath}/${path}`
 }
