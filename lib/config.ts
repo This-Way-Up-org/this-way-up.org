a0881c7981442855cb5735046b7192d92299219b
Offren <OffrenLLC@outlook.com>
2024-10-30 21:31:20 -0500

    added blame URL for the history button

--- lib/config.ts
+++ lib/config.ts
@@ -1,11 +1,14 @@
 export const GITHUB_REPO = {
-    owner: 'Offren',  // replace this with your Github Username
-    name: 'gitWiki', // replace this with your repo's name
-    branch: 'main',
-    contentPath: 'content'
-  }
-  
-  export const getGithubEditUrl = (path: string): string => {
-    return `https://github.com/${GITHUB_REPO.owner}/${GITHUB_REPO.name}/edit/${GITHUB_REPO.branch}/${GITHUB_REPO.contentPath}/${path}`
-  }
-  
+  owner: 'Offren',  // replace this with your Github Username
+  name: 'gitWiki', // replace this with your repo's name
+  branch: 'main',
+  contentPath: 'content'
+}
+
+export const getGithubEditUrl = (path: string): string => {
+  return `https://github.com/${GITHUB_REPO.owner}/${GITHUB_REPO.name}/edit/${GITHUB_REPO.branch}/${GITHUB_REPO.contentPath}/${path}`
+}
+
+export const getGithubBlameUrl = (path: string): string => {
+  return `https://github.com/${GITHUB_REPO.owner}/${GITHUB_REPO.name}/blame/${GITHUB_REPO.branch}/${GITHUB_REPO.contentPath}/${path}`
+}
