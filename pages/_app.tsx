c5ca4f52b4501894fa16f17b109ee7f3460cac79
Awesomeautism <OffrenLLC@outlook.com>
2024-11-01 01:32:44 -0500

    resolving conflict markers in your pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
+// pages/_app.tsx
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a243f39ecbfbb85c3830f27c285771230bed5ef6
