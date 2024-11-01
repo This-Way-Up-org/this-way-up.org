ed9383bb71f5b4bcfe890f8716300a8faaef68ab
Awesomeautism <OffrenLLC@outlook.com>
2024-11-01 01:12:33 -0500

    more fixes to display footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+// pages/_app.tsx
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { AppProps } from 'next/app'
+import Layout from '../components/Layout'; // Ensure this path is correct and component exists
+import '../styles/globals.css'; // Ensure this file exists
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
+
+export default MyApp
